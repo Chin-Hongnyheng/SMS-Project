ad2393f17a1d66ac292794f956108f07105cdf54
Chin Hongnyheng <hongnyhengchin@gmail.com>
2026-02-04 13:09:53 +0700

    Updated

--- SMS Project/backend/src/curriculum/entities/curriculum.entity.ts
+++ SMS Project/backend/src/curriculum/entities/curriculum.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeOrm';
 import { Assignment } from './assignment.entity';
 import { Lecture } from './lecture.entity';
 import { Announcement } from './announcement.entity';
