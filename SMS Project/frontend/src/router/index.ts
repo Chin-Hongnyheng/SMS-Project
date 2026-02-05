3f37ac45cbee3c8999f44803fc608b7fffba7f33
Poppy Khim <mobilelegendpoppykhim@gmail.com>
2026-02-06 03:31:55 +0700

    fixed

--- SMS Project/frontend/src/router/index.ts
+++ SMS Project/frontend/src/router/index.ts
@@ -113,12 +113,6 @@ const router = createRouter({
       meta: { requiresAuth: true, roles: ["Student", "Teacher", "Admin"] },
       component: () => import("../views/AccountView.vue"),
     },
-    {
-      path: "/AhHengKeBanh",
-      name: "AhHengKeBanh",
-      meta: { requiresAuth: true, roles: ["Student", "Teacher", "Admin"] },
-      component: () => import("../views/AccountView.vue"),
-    },
   ],
 });
 router.beforeEach((to, from, next) => {
